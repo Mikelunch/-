0b6ca5e61a9de32957117c6d6929ee9fd8b68508
ConstnatZ <66398046+Mikelunch@users.noreply.github.com>
2023-11-26 21:17:44 +0800

    Add files via upload
    二分法的算法模板

---
+++ 算法3：二分法/main.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int N = 100000;
+int nums[N];
+
+int main() {
+    int n, q;
+    int l_bound, r_bound;
+    cin >> n >> q;
+    for (int i = 0; i < n; i++) scanf("%d", &nums[i]);
+
+    while (q--) {
+        int num;
+        cin >> num;
+
+        int l = 0, r = n - 1;
+        while (l < r) {
+            int mid = (l + r + 1) >> 1; //第一步确定当前二分点
+            if (nums[mid] <= num) l = mid;
+            else r = mid - 1;
+        }
+        if (nums[l] != num) {
+            cout << "-1 -1" << endl;
+            continue;
+        }
+        else {
+            r_bound = r;
+            l = 0, r = n - 1;
+            while (l < r) {
+                int mid = (l + r) >> 1;
+                if (nums[mid] >= num) r = mid;
+                else l = mid + 1;
+            }
+            l_bound = l;
+        }
+        cout << l_bound << " " << r_bound << endl;
+    }
+    return 0;
+}
---
+++ 算法3：二分法/main_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+#define slope 1e-8
+
+int main() {
+    double num;
+    cin >> num;
+    int cnt = 0;
+    double l, r;
+    if (num >= 0) l = 0, r = num * 10000; //防止有绝对值小于1的数进行立方根而超范围 如0.001的立方根是0.1 而0.1不在0，0001的范围内
+    else l = num * 10000, r = 0;
+
+    while (r - l > slope) {
+        double mid = (l + r) / 2;
+        if (pow(mid, 3) >= num) r = mid;
+        else l = mid;
+    }
+
+    printf("%6f", r);
+
+    return 0;
+}
