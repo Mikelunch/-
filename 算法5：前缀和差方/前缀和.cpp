914e963d3eb9c77a756499ff1f8646df6b256999
ConstnatZ <66398046+Mikelunch@users.noreply.github.com>
2023-12-07 23:25:17 +0800

    Add files via upload
    算法5：前缀和差分

---
+++ 算法5：前缀和差方/前缀和.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+const int N = 1e7;
+int s[N], nums[N]; //在这里定义前缀和数组则不需要使用s0 = 0 操作
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &nums[i]); //注意这里需要从1开始赋值
+        s[i] = s[i - 1] + nums[i];//注意这里需要从1开始赋值
+    }
+
+    while (m--) {
+        int l, r;
+        scanf("%d %d", &l, &r);
+        printf("%d\n", s[r] - s[l - 1]);
+    }
+
+    return 0;
+}
+
+作者：ConstantZ
+链接：https ://www.acwing.com/activity/content/code/content/7479156/
+来源：AcWing
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
---
+++ 算法5：前缀和差方/差分.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+const int N = 1e7;
+int a[N], b[N];
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
+
+    for (int i = 1; i <= n; i++) { //根据前缀和数组a构造a的差方数组b
+        b[i] += a[i];      //这里可以理解为b一开始是0 a数组的构造其实是在b的当前位置进行插入操作
+        b[i + 1] -= a[i];
+    }
+
+    while (m--) {
+        int l, r, c;
+        scanf("%d %d %d", &l, &r, &c);
+        b[l] += c; //根据差分和的性质可以得出：前缀和某一段上的加法操作可以由差分数组完成
+        b[r + 1] -= c;
+    }
+
+    for (int i = 1; i <= n; i++) b[i] += b[i - 1]; //使得差分转换为前缀和输出
+
+    for (int i = 1; i <= n; i++) printf("%d ", b[i]);
+
+    return 0;
+}
