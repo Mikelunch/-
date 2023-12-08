bbd12e6d0c99f31859d1f880762a9660d30ba02f
ConstnatZ <66398046+Mikelunch@users.noreply.github.com>
2023-12-08 20:45:02 +0800

    Add files via upload
    算法7：位运算

---
+++ 算法7：位运算/位运算.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int lowbit(int x) {
+    return x & -x;
+}
+
+int main() {
+    int n, num;
+    cin >> n;
+    while (n--) {
+        //cin >> num;
+        scanf("%d", &num);
+        int res = 0;
+        while (num) num -= lowbit(num), res++; //核心 每次去掉对后一位1即可
+        //cout << res << " ";
+        printf("%d ", res);
+    }
+
+    return 0;
+}
