437fbed396de67303920f51e664201ccea85e173
ConstnatZ <66398046+Mikelunch@users.noreply.github.com>
2024-01-09 21:57:17 +0800

    Add files via upload

---
+++ 算法11：DFS/843_皇后问题.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+using namespace std;
+
+const int N = 10;
+int p[N];//显示用数组
+bool st[N];//记录每个数的使用情况
+int n;
+
+void dfs(int u) {
+    if (u == n) {//满足深度则输出并返回
+        for (int i = 0; i < n; i++) printf("%d ", p[i]);
+        printf("\n");
+        return;
+    }
+    for (int i = 1; i <= n; i++) {//没由达到深度条件则继续向下搜索
+        if (!st[i]) {//该数字并没有被使用
+            p[u] = i;//该层使用这个数字
+            st[i] = true;//记录该位上的数字已被使用下一层不能使用
+            dfs(u + 1);//向下一层搜索
+            st[i] = false;//返回时要进行回溯并恢复现场
+        }
+    }
+}
+
+int main() {
+
+    cin >> n;
+
+    dfs(0);
+
+    return 0;
+
+}
+
+作者：ConstantZ
+链接：https ://www.acwing.com/activity/content/code/content/7589678/
+来源：AcWing
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
---
+++ 算法12：BFS/BFS.cpp
@@ -0,0 +1,53 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<cstring>
+
+using namespace  std;
+
+typedef pair<int, int> PII;
+const int N = 101;
+int d[N][N], g[N][N]; //d存储对应坐标到起始点的距离 g是迷宫数据
+PII q[N * N];//存储坐标
+int hh, tt, m, n;
+
+void init() {//初始化各种参数
+    hh = tt = 0;//先保证队列非空
+    memset(d, -1, sizeof(d));//标记每个点都是未走过的状态
+    d[0][0] = 0;//标记起始点是默认走过的状态
+    for (int i = 0; i < n; i++)//输入迷宫数据
+        for (int j = 0; j < m; j++) scanf("%d", &g[i][j]);
+}
+
+int bfs() {
+    q[0] = { 0 , 0 };// 第一步：将初始状态插入队头
+    int dx[4] = { 0 ,0 ,1 ,-1 }, dy[4] = { 1 , -1 ,0 ,0 };//用2个方向数组来代表四个前进方向 可参考ACWING765
+    while (hh <= tt) { // 只要队列非空就一直进行
+        auto t = q[hh++]; // 第二步：取出队头元素
+        for (int i = 0; i < 4; i++) {//四个方向都要遍历一边以确定下一层所有可能的点
+            int x = t.first + dx[i], y = t.second + dy[i];//四个方向遍历
+            if (x >= 0 && x < n && y >= 0 && y < n && d[x][y] == -1 && g[x][y] == 0) {
+                d[x][y] = d[t.first][t.second] + 1;//符合条件的点距离上一层的距离+1
+                q[++tt] = { x , y };//存储符合条件的坐标
+            }
+        }
+    }
+    return d[n - 1][m - 1];
+}
+
+int main() {
+    cin >> n >> m;
+    init();
+    cout << bfs() << endl;
+    /* 展示距离用
+    for(int i = 0 ; i < n; i++){
+        for(int j = 0 ; j < m; j++) printf("%d ", d[i][j]);
+        printf("\n");
+    }
+    */
+    return 0;
+}
+
+作者：ConstantZ
+链接：https ://www.acwing.com/activity/content/code/content/7590755/
+来源：AcWing
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
