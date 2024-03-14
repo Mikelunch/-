7bc5fcb7ca91182bca43561b271ede71229bfbf0
ConstnatZ <66398046+Mikelunch@users.noreply.github.com>
2024-03-14 21:50:38 +0800

    Add files via upload

---
+++ ACWING算法基础课/第三部分：图论和搜索/算法18：spfa算法/spfa判断负环.cpp
@@ -0,0 +1,60 @@
+﻿#include<iostream>
+#include<queue>
+#include<cstring>
+using namespace std;
+
+const int N = 100086;
+int h[N], e[N], ne[N], w[N], idx;
+queue<int> q;
+bool s[N];
+int d[N], cnt[N];
+int n, m;
+
+void init(void) {
+    memset(h, -1, sizeof h);
+    idx = 0;
+}
+
+void add(int a, int b, int c) {
+    e[idx] = b, ne[idx] = h[a], h[a] = idx, w[idx] = c, idx++;
+}
+
+bool spfa(void) {
+    for (int i = 1; i <= n; i++) {
+        s[i] = true;
+        q.push(i);
+    }
+
+    while (q.size()) {
+        auto t = q.front();
+        q.pop();
+        s[t] = false;
+        for (int i = h[t]; ~i; i = ne[i]) {
+            int j = e[i];
+            if (d[j] > d[t] + w[i]) {
+                d[j] = d[t] + w[i];
+                cnt[j] = cnt[t] + 1;
+                if (cnt[j] >= n) return true;
+                if (!s[j]) {
+                    q.push(j);
+                    s[j] = true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+int main() {
+    cin >> n >> m;
+    init();
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+        add(a, b, c);
+    }
+    if (spfa()) printf("Yes");
+    else printf("No");
+
+    return 0;
+}
---
+++ ACWING算法基础课/第三部分：图论和搜索/算法18：spfa算法/spfa求最短路.cpp
@@ -0,0 +1,58 @@
+﻿#include<iostream>
+#include<queue>
+#include<cstring>
+using namespace std;
+
+const int N = 100086;
+int h[N], e[N], ne[N], w[N], idx;
+queue<int> q;
+bool s[N];
+int d[N];
+int n, m;
+
+void init(void) {
+    memset(h, -1, sizeof h);
+    idx = 0;
+}
+
+void add(int a, int b, int c) {
+    e[idx] = b, ne[idx] = h[a], h[a] = idx, w[idx] = c, idx++;
+}
+
+int spfa(void) {
+    memset(d, 0x3f, sizeof d);
+    d[1] = 0;
+    q.push(1);
+    s[1] = true;
+    while (q.size()) {
+        auto t = q.front();
+        q.pop();
+        s[t] = false;
+        for (int i = h[t]; ~i; i = ne[i]) {
+            int j = e[i];
+            if (d[j] > d[t] + w[i]) {
+                d[j] = d[t] + w[i];
+                if (!s[j]) {
+                    q.push(j);
+                    s[j] = true;
+                }
+            }
+        }
+    }
+    return d[n];
+}
+
+int main() {
+    cin >> n >> m;
+    init();
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+        add(a, b, c);
+    }
+    int t = spfa();
+    if (d[n] == 0x3f3f3f3f) printf("impossible");
+    else printf("%d", d[n]);
+
+    return 0;
+}
